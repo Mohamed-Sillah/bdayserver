e8b49dae1fce1c0ff7cdfb815fdf5457e293538b
Abubakarr Marah <telastudio2023@gmail.com>
2025-10-21 19:40:34 +0000

    added_an_origin

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://samuellaspecialday.netlify.app"
+    "https://samuellaspecialday.netlify.app",
+    "https://elladay.netlify.app"
 ];
 
 export default allowedOrigins;
